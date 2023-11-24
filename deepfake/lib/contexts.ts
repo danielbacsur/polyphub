5a3f6d8375512e89822520d9bb6d717ae7099dac
Daniel Bacsur <contact@danielbacsur.com>
2023-11-24 22:21:58 +0100

    Create contexts

--- deepfake/lib/contexts.ts
+++ deepfake/lib/contexts.ts
@@ -1,4 +1,8 @@
-import { type User } from "@prisma/client";
+import { type Validation, type User, type Tag } from "@prisma/client";
 import { createContext } from "react";
 
 export const UserContext = createContext<User | null>(null);
+
+export const ValidationContext = createContext<Validation | null>(null);
+
+export const TagContext = createContext<Tag | null>(null);
