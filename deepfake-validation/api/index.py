085a31aaf92bcfc1e9473aca331293cc3881797a
Daniel Bacsur <contact@danielbacsur.com>
2023-11-25 11:08:12 +0100

    Update index.py

--- deepfake-validation/api/index.py
+++ deepfake-validation/api/index.py
@@ -1,18 +1,12 @@
 from flask import Flask, request, jsonify
-
-from utils import *
-
-import requests
 import multiprocessing
-
-import json
+from utils import *
+import requests
 
 
 def analyze_video( url: str) -> int:
     video_path = download_video(url)
     frames, frame_rate = extract_frames(video_path)
-    if len(frames) == 0:
-        return jsonify({'error': 'no-frames'}), 400
 
     face_detector = DlibFaceDetector()
     
@@ -21,38 +15,38 @@ def analyze_video( url: str) -> int:
     # Check for frame inconsistencies
     frame_inconsistencies = check_frame_consistency(frames, frame_rate)
     if len(frame_inconsistencies) > 0:
-        print("[❗] Found frame inconsistencies")
+        print("Found frame inconsistencies")
         print(frame_inconsistencies)
     else:
-        print("[✅] No frame inconsistencies found")
+        print("No frame inconsistencies found")
 
     # Check for face count inconsistencies
     face_inconsistencies = check_face_consistency(frames, frame_rate, face_detector)
     if len(face_inconsistencies) > 0:
-        print("[❗] Found face inconsistencies")
+        print("Found face inconsistencies")
         print(face_inconsistencies)
     else:
-        print("[✅] No face inconsistencies found")
+        print("No face inconsistencies found")
 
     # Check for brightness and contrast inconsistencies
     brightness_contrast_inconsistencies = check_brightness_contrast_consistency(frames, frame_rate)
     if len(brightness_contrast_inconsistencies) > 0:
-        print("[❗] Found brightness and contrast inconsistencies")
+        print("Found brightness and contrast inconsistencies")
         print(brightness_contrast_inconsistencies)
     else:
-        print("[✅] No brightness and contrast inconsistencies found")
+        print("No brightness and contrast inconsistencies found")
 
     # Check for blinks
     blink_frames = detect_blinks(frames, frame_rate, face_detector)
     blink_error_type = "nominal"
     if len(blink_frames) == 0:
-        print("[❗] No blinks found")
+        print("No blinks found")
         blink_error_type = "no-blinks"
     elif len(blink_frames) > len(frames) / frame_rate * 1.5 and len(blink_frames) > 2:
-        print("[❗] Too many blinks found")
+        print("Too many blinks found")
         blink_error_type = "too-many-blinks"
     else:
-        print("[✅] No blink inconsistencies found")
+        print("No blink inconsistencies found")
     
     print("Blinks:", len(blink_frames))
 
@@ -82,8 +76,6 @@ def analyze_video( url: str) -> int:
         "duration": len(frames) / frame_rate,
         "blinks": blink_error_type
     }
-
-
 
 app = Flask(__name__)
 
