8f7fc2f5a32147fe607e36961fb6c3e7d669c56e
Daniel Bacsur <contact@danielbacsur.com>
2023-11-26 01:08:18 +0100

    Probability and dots correction

--- deepfake/app/app/(dashboard)/overview/page.tsx
+++ deepfake/app/app/(dashboard)/overview/page.tsx
@@ -5,6 +5,7 @@ import { getValidation } from "@/lib/fetchers/validation";
 import { type Validation } from "@/lib/types/prisma";
 import * as Table from "@/components/ui/table";
 import { useEffect, useRef } from "react";
+import { Progress } from "@/components/ui/progress";
 
 export default function OverviewPage() {
   const { validation, setValidation } = useValidation();
@@ -39,8 +40,22 @@ export default function OverviewPage() {
         Sorry you screen is too small.
       </div>
       <div className="hidden lg:flex h-full flex-col">
-        <div className="flex-grow pl-[calc(40vw_+_1rem)] pr-4 py-4 -mb-4">
-          <div className="bg-black w-full h-full rounded-lg relative overflow-hidden">
+        <div className="flex-grow flex p-4 -mb-4 space-x-4">
+          <div className="flex-none h-full w-[calc(40vw_-_1rem)] rounded-lg grid place-items-center">
+            <div className="w-full flex flex-col items-center space-y-8">
+              <h1 className="text-3xl font-bold">Probability</h1>
+
+              <Progress
+                value={(validation.metadata?.probability || 0) * 100}
+                className="w-[60%] h-4"
+              />
+
+              <h1 className="text-3xl font-bold">{`${(
+                (validation.metadata?.probability || 0) * 100
+              ).toFixed()} %`}</h1>
+            </div>
+          </div>
+          <div className="flex-grow bg-black w-full h-full rounded-lg relative overflow-hidden">
             <video
               className="absolute bottom-0 min-h-full min-w-full"
               loop
@@ -93,22 +108,24 @@ export default function OverviewPage() {
                       100
                     ).toFixed(1)}
                   </Table.TableCell>
-                  <Table.TableCell className="pl-[2px] pr-0 relative border-x">
-                    {tag.times.map((time) => {
-                      const left = getTimelinePercent(
-                        time,
-                        validation,
-                        tag.times
-                      );
+                  <Table.TableCell className="px-1 border-x">
+                    <div className="bg-red-500 w-full h-0 relative">
+                      {tag.times.map((time) => {
+                        const left = getTimelinePercent(
+                          time,
+                          validation,
+                          tag.times
+                        );
 
-                      return (
-                        <div
-                          key={time}
-                          className="absolute w-2 h-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black"
-                          style={{ left }}
-                        />
-                      );
-                    })}
+                        return (
+                          <div
+                            key={time}
+                            className="absolute w-2 h-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-black"
+                            style={{ left }}
+                          />
+                        );
+                      })}
+                    </div>
                   </Table.TableCell>
                 </Table.TableRow>
               ))}
