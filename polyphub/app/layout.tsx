e3f32036d37fef28beb42914ad0a7eb9480dc661
Daniel Bacsur <contact@danielbacsur.com>
2023-11-26 03:48:47 +0100

    Update main page

--- polyphub/app/layout.tsx
+++ polyphub/app/layout.tsx
@@ -1,19 +1,16 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { type ReactNode } from "react";
+import { type Metadata } from "next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Polyphub",
+  title: "Polyp HUB",
   description: "Discover our solutions for the Junction X Budapest hackathon",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
--- polyphub/app/page.tsx
+++ polyphub/app/page.tsx
@@ -1,21 +1,15 @@
-import { SelectionButton } from "@/components/selection-button";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import Balancer from "react-wrap-balancer";
 
 export default function Home() {
   return (
     <>
       <div className="h-full flex flex-col lg:flex-row items-center pt-12 md:pt-0">
         <div className="flex-1 grid place-items-center">
-          <SelectionButton
-            title="Deepfake"
-            description="Ever watched a video and wondered if it's a celebrity or their digital twin speaking?"
-            buttonHref="https://app.deepfake.polyphub.hu/"
-            buttonText="Check if you are a Deepfake"
-          />
-        </div>
-        <div className="flex-none grid place-items-center order-first lg:order-none">
           <Image
-            className="w-48 lg:w-auto"
+            className="w-auto"
             src="/logo.png"
             alt="Polyphub logo"
             width={300}
@@ -23,12 +17,20 @@ export default function Home() {
           />
         </div>
         <div className="flex-1 grid place-items-center">
-          <SelectionButton
-            title="Radiotherapy"
-            description="Imagine a world where radiotherapy appointments are as easy to schedule as your favorite TV show."
-            buttonHref="https://app.radiotherapy.polyphub.hu/"
-            buttonText="Discover Our 3D Hospital"
-          />
+          <div className="flex flex-col space-y-4 lg:space-y-8 text-center">
+            <h1 className="text-2xl lg:text-3xl font-bold">Deepfake</h1>
+
+            <p className="w-96">
+              <Balancer>
+                Ever watched a video and wondered if it&apos;s a celebrity or their
+                digital twin speaking?
+              </Balancer>
+            </p>
+
+            <Link href="https://app.deepfake.polyphub.hu/">
+              <Button>Check if you are a Deepfake</Button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
